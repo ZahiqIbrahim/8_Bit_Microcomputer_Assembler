ecfa5482e919742f41f049fe5bd46e223e6fbd15
Zahiq Ibrahim <161963558+ZahiqIbrahim@users.noreply.github.com>
2026-06-01 21:54:26 +0530

    Fixed some naming issues

--- src/main/java/com/example/__Bit_Microcontrolller_Assembler/controller/ProgramController.java
+++ src/main/java/com/example/__Bit_Microcontrolller_Assembler/controller/ProgramController.java
@@ -5,12 +5,13 @@ import com.example.__Bit_Microcontrolller_Assembler.service.Assembler;
 import com.example.__Bit_Microcontrolller_Assembler.service.SerialPortCom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class ProgramController {
 
